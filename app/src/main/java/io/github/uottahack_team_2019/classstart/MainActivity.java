31f02f6602cbcead25686a3fc66f8c3f1d2a35ec
Chris Vettese <cvett018@uottawa.ca>
2019-02-09 15:14:13 -0500

    File code

--- app/src/main/java/io/github/uottahack_team_2019/classstart/MainActivity.java
+++ app/src/main/java/io/github/uottahack_team_2019/classstart/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FileManager fileManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         setStatusBarColour();
+
+        fileManager = new FileManager(this);
 
         setContentView(R.layout.activity_main);
     }
