da53c373ba71d62b4b5d80bb0f3c657bdae7ac03
Chris Vettese <cvett018@uottawa.ca>
2019-02-09 19:57:34 -0500

    jason's code

---
+++ app/src/main/java/io/github/uottahack_team_2019/classstart/Course.java
@@ -0,0 +1,7 @@
+package io.github.uottahack_team_2019.classstart;
+
+public class Course {
+    public Course(MainActivity activity) {
+        activity.setContentView(R.layout.activity_course);
+    }
+}
